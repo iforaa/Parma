2738938d40983ccb29b27c99ffbd8ec35935d446
igor <igor.n.kuz@gmail.com>
2021-04-28 17:03:46 +0300

    Update down dependency

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
             targets: ["Parma"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/iwasrobbed/Down", from: "0.9.3")
+        .package(url: "https://github.com/johnxnguyen/Down", from: "0.10.0")
     ],
     targets: [
         .target(
